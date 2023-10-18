7697f7cde57709796f5f03f6c709e076e6bd0a3f
Bayan999 <b-nayf@hotmail.com>
2023-10-18 11:03:45 +0300

    99

---
+++ Truwa3/OnboardingVe.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingVe.swift
+//  Truwa3
+//
+//  Created by Bayan on 03/04/1445 AH.
+//
+
+import SwiftUI
+
+struct OnboardingVe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingVe()
+}
