102dbb1a5e72d92a13a1dc173499fe13aff2a6dc
Bayan999 <b-nayf@hotmail.com>
2023-10-18 10:45:44 +0300

    Initial Commit

---
+++ Truwa3/Truwa3App.swift
@@ -0,0 +1,17 @@
+//
+//  Truwa3App.swift
+//  Truwa3
+//
+//  Created by Bayan on 03/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Truwa3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
